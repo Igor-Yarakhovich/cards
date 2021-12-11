7dcf4376b0e900709dc3fc27f1916f9d6696dfe6
Beergrizl <a.sadovskii@tut.by>
2021-12-12 00:58:31 +0300

    some UI, instance, thunk, server request-response

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import {Main} from "../features/main/Main";
 import {Navbar} from "../features/navbar/Navbar";
 
-//hello everybody  1111111111111111
+
+
 
 const App = () => {
     return (
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,70 @@
 import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {useFormik} from "formik";
+import {FormikErrorType} from "./loginPage-api";
+import {loginTC} from "./loginReducer";
+import {Navigate} from "react-router-dom";
+import {AppRootStateType} from "../../app/store";
 
 export const Login: React.FC = () => {
+
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)
+    const dispatch = useDispatch()
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: Partial<FormikErrorType> = {};
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length>15) {
+                errors.password = 'Your password should be not longer then 15 characters';
+            }
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            formik.resetForm();
+            dispatch(loginTC(values))
+        }
+    })
+
+    if (isLoggedIn) {
+        return <Navigate to='/profile'/>
+    }
     return (
         <div>
-            Login
+            <h1> IT-incubator</h1>
+            <p>Sign in</p>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <input type={"email"} placeholder={'email'}
+                           {...formik.getFieldProps('email')}/>
+                </div>
+                {formik.touched.email &&
+                formik.errors.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+                <div>
+                    <input type={"password"} placeholder={'password'}
+                           {...formik.getFieldProps('password')}/>
+                </div>
+                {formik.touched.password &&
+                formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+                <div>
+                    <input type={'checkbox'}/> Remember me
+                </div>
+                <a href={'#/registration'}> passwordRecovery</a>
+                <div>
+                    <button type={'submit'}>login</button>
+                </div>
+                <a href={'#/passwordRecovery'}> Sign up</a>
+            </form>
         </div>
     )
 }
