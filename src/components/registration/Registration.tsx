720e14c3c90262c0933c4431061eb8c3f23f5e78
Evgeny Kreger <evgeny_kreger@mail.ru>
2021-12-12 14:35:08 +0300

    jsx markup

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import style from './Registration.module.css'
 
 export const Registration: React.FC = () => {
+
     return (
-        <div>
-            Registration
+        <div className={style.main}>
+           <div><h2>It-incubator</h2></div>
+           <div><h3>Sign-up</h3></div>
+           <div><input type="email" placeholder={'Email'}/></div>
+           <div><input type="password" placeholder={'Password'}/><button>X</button></div>
+           <div><input type="password" placeholder={'Confirm password'}/><button >X</button></div>
+           <div><button disabled={false}>Cancel</button><button disabled={false}>Register</button></div>
         </div>
     )
 }
-//////test
+
