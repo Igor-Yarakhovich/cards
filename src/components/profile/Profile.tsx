c427d89b2bda80ee3a42523a6365899220b3c2e0
Beergrizl <a.sadovskii@tut.by>
2021-12-16 11:46:41 +0300

    profile logOut

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,26 +1,51 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
 import gomer from "../login/gomer.jpg"
-import {setNameAC} from "../login/loginReducer";
-import {EditableSpan} from "../login/editableSpan";
+import {initialiseTC, logOutTC, newNameTC} from "./profileReducer";
+import {Preloader} from "../login/Preloader";
+import {ErrorType} from "../login/loginReducer";
+import {EditableSpan} from "./editableSpan";
+import {Navigate} from "react-router-dom";
 
 export const Profile: React.FC = () => {
     const dispatch = useDispatch()
-    const email = useSelector<AppRootStateType, string>(state => state.login.data.email)
-    const photo = useSelector<AppRootStateType, string | undefined>(state => state.login.data.avatar)
-    const name = useSelector<AppRootStateType, string>(state => state.login.data.name)
+    const email = useSelector<AppRootStateType, string>(state => state.profile.data.email)
+    const photo = useSelector<AppRootStateType, string | undefined>(state => state.profile.data.avatar)
+    const name = useSelector<AppRootStateType, string>(state => state.profile.data.name)
+    const status = useSelector<AppRootStateType, string>(state => state.login.status)
+    const error = useSelector<AppRootStateType, ErrorType>(state => state.login.error)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)
+    const initialised = useSelector<AppRootStateType, boolean>(state => state.profile.initialised)
+
+    useEffect(() => {
+        dispatch(initialiseTC())
+    }, [])
+
 
     const startValueHandler = useCallback((title: string) => {
-        dispatch(setNameAC(title))
-    }, [dispatch, name])
+        dispatch(newNameTC(title))
+    }, [dispatch])
+
+    const logOutHandler = () => {
+        dispatch(logOutTC())
+    }
+    if (!initialised) {
+        return <Preloader/>
+    }
+
+    if (!isLoggedIn) {
+        return <Navigate to='/login'/>
+    }
 
     return (
         <div>
             <h1>Personal information</h1>
+            {error ? error : (status === 'loading') && <Preloader/>}
             <div> {photo ? photo : <img src={gomer}/>}</div>
             <div>email: {email}  </div>
             <div>name: <EditableSpan value={name} onChange={startValueHandler}/></div>
+            <button onClick={logOutHandler}>LogOut</button>
         </div>
     )
 }
--- src/components/profile/profileReducer.ts
+++ src/components/profile/profileReducer.ts
@@ -1,19 +1,103 @@
-const initialState = {};
+import {Dispatch} from "redux";
+import {
+    setAppErrorAC, SetAppErrorActionType,
+    setIsLoggedInAC,
+    SetIsLoggedInType,
+    setStatusAC,
+    SetStatusType
+} from "../login/loginReducer";
+import {meAPI} from "./profilePage-api";
+import {ResponseType} from "../login/loginPage-api";
 
-export const profileReducer = (state: InitialStateType = initialState, action: AppActionType): InitialStateType => {
+const initialState = {
+    data: {} as ResponseType,
+    initialised: false
+};
+
+export const profileReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
-        case "APP/SET-STATUS":
-            return state;
+        case "profile/SET-DATA":
+            return {...state, data: action.data}
+        case "profile/SET-NAME":
+            return {...state, data: {...state.data, name: action.name}}
+        case "profile/SET-INITIALISED":
+            return {...state, initialised: action.initialised}
         default:
             return state;
     }
 };
 
 // actions
-const setAppStatusAC = () => ({type: "APP/SET-STATUS"} as const)
-
+export const isInitialisedAC = (initialised: boolean) =>
+    ({type: 'profile/SET-INITIALISED', initialised} as const)
+export const setNameAC = (name: string) =>
+    ({type: 'profile/SET-NAME', name} as const)
+export const setDataAC = (data: ResponseType) =>
+    ({type: 'profile/SET-DATA', data} as const)
 // thunks
-
+export const initialiseTC = () => (dispatch: Dispatch<ActionType>) => {
+    dispatch(setStatusAC('loading'))
+    meAPI.me()
+        .then(res => {
+            if (res.data) {
+                dispatch(setIsLoggedInAC(true))
+                dispatch(setStatusAC('succeeded'))
+                dispatch(setDataAC(res.data))
+                dispatch(isInitialisedAC(true))
+            }
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console');
+            dispatch(setAppErrorAC(error))
+        })
+}
+export const newNameTC = (name: string) => (dispatch: Dispatch<ActionType>) => {
+    dispatch(setStatusAC('loading'))
+    meAPI.setNewName(name)
+        .then(res => {
+            debugger
+            if (res.data) {
+                dispatch(setStatusAC('succeeded'))
+                dispatch(setNameAC(res.data.updatedUser.name))
+            }
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console');
+            dispatch(setAppErrorAC(error))
+        })
+}
+export const logOutTC = () => (dispatch: Dispatch<ActionType>) => {
+    dispatch(setStatusAC('loading'))
+    meAPI.logOut()
+        .then(res => {
+            if (res.data) {
+                dispatch(setIsLoggedInAC(false))
+            }
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console');
+            dispatch(setAppErrorAC(error))
+        })
+        .finally(() => {
+                dispatch(setStatusAC('succeeded'))
+            }
+        )
+}
 // types
+export type SetInitialisedType = ReturnType<typeof isInitialisedAC>
+export type SetDataType = ReturnType<typeof setDataAC>
+export type SetNameType = ReturnType<typeof setNameAC>
 type InitialStateType = typeof initialState;
-export type AppActionType = ReturnType<typeof setAppStatusAC>
+export type ActionType =
+    SetInitialisedType
+    | SetDataType
+    | SetNameType
+    | SetIsLoggedInType
+    | SetStatusType
+    | SetAppErrorActionType
