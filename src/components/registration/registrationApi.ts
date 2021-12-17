66a96a605bf275c4952d9c8cf033d5dad9b3d3e9
Igor-Yarakhovich <igtopor@yandex.ru>
2021-12-17 12:57:03 +0300

    add url heroky

--- src/components/registration/registrationApi.ts
+++ src/components/registration/registrationApi.ts
@@ -10,7 +10,7 @@ type RegisterResponseType = {
 }
 
 const instance = axios.create({
-    baseURL:"http://localhost:7542/2.0/",
+    baseURL:"https://neko-back.herokuapp.com/2.0/",
     withCredentials:true
 })
 
