1b837c43cce947fae6a5dafb3e782bc982f44ddd
Beergrizl <a.sadovskii@tut.by>
2021-12-21 00:52:38 +0300

    updates

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,15 +1,23 @@
 import React, {useCallback, useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
-import gomer from "../login/gomer.jpg"
+import avatar from "../../assets/images/avatar.png"
 import {initialiseTC, logOutTC, newNameTC} from "./profileReducer";
-import {Preloader} from "../login/Preloader";
-import {ErrorType, setAppErrorAC} from "../login/loginReducer";
+
+import {Preloader} from "../../assets/Preloader/Preloader";
+import {ErrorType} from "../login/loginReducer";
+
 import {EditableSpan} from "./editableSpan";
-import {Navigate} from "react-router-dom";
+import {Login} from "../login/Login";
 
 export const Profile: React.FC = () => {
+
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(initialiseTC())
+    }, [dispatch])
+
     const email = useSelector<AppRootStateType, string>(state => state.profile.data.email)
     const photo = useSelector<AppRootStateType, string | undefined>(state => state.profile.data.avatar)
     const name = useSelector<AppRootStateType, string>(state => state.profile.data.name)
@@ -18,11 +26,6 @@ export const Profile: React.FC = () => {
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)
     const initialised = useSelector<AppRootStateType, boolean>(state => state.profile.initialised)
 
-    useEffect(() => {
-        dispatch(initialiseTC())
-    }, [])
-
-
     const startValueHandler = useCallback((title: string) => {
         dispatch(newNameTC(title))
     }, [dispatch])
@@ -30,12 +33,12 @@ export const Profile: React.FC = () => {
     const logOutHandler = () => {
         dispatch(logOutTC())
     }
-    if (!initialised && isLoggedIn) {
+    if (!initialised ) {
         return <Preloader/>
     }
 
     if (!isLoggedIn) {
-        return <Navigate to='/login'/>
+        return <Login/>
     }
 
     /*useEffect(()=>{
@@ -50,7 +53,7 @@ export const Profile: React.FC = () => {
         <div>
             <h1>Personal information</h1>
             {error && initialised ? error : (status === 'loading') && <Preloader/>}
-            <div> {photo ? photo : <img src={gomer}/>}</div>
+            <div> {photo ? <img alt='' src={photo}/> : <img alt='' src={avatar}/>}</div>
             <div>email: {email}  </div>
             <div>name: <EditableSpan value={name} onChange={startValueHandler}/></div>
             <button onClick={logOutHandler}>LogOut</button>
