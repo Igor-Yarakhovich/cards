2fe63d94c492b53ffdfacb28625d7901e1304607
Klimovich Yauhenia <zh.shuravina@gmail.com>
2021-12-21 23:52:53 +0300

    new style app, new structure and style navbar commit

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -5,26 +5,37 @@ import {NavLink} from "react-router-dom";
 export const Navbar: React.FC = () => {
 
     return <nav className={styles.nav}>
-        <div>
-            <NavLink to='/profile'
-                     className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
-            >Profile</NavLink>
-        </div>
-        <div>
-            <NavLink to='/login'
-                     className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
-            >Login</NavLink>
-        </div>
-        <div>
-            <NavLink to='/registration'
-                     className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
-            >Registration</NavLink>
-        </div>
-        <div>
-            <NavLink to='/passwordRecovery'
-                     className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
-            >PasswordRecovery</NavLink>
-        </div>
-    </nav>
+                <div className={styles.container}>
+                    <div className={styles.navWrapper}>
+
+                        <div>
+                            <h2 className={styles.navLogo}>It-incubator</h2>
+                        </div>
+                        <div>
+                            <NavLink to='/profile'
+                                    className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
+                            >Profile</NavLink>
+                        </div>
+                        <div className={styles.navRight}>
+                            
+                            <div>
+                                <NavLink to='/login'
+                                        className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
+                                >Login</NavLink>
+                            </div>
+                            <div>
+                                <NavLink to='/registration'
+                                        className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
+                                >Registration</NavLink>
+                            </div>
+                            <div>
+                                <NavLink to='/passwordRecovery'
+                                        className={({isActive}) => (isActive ? styles.activeStyle : styles.item)}
+                                >PasswordRecovery</NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
 }
